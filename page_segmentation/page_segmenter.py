cb3f36b5e11b9d08d5d822825b911a3e580381cf
joyantabasak13 <joyantabasak13@users.noreply.github.com>
2020-09-27 00:46:31 +0600

    Pdf2Image added

--- page_segmentation/page_segmenter.py
+++ page_segmentation/page_segmenter.py
@@ -1,5 +1,8 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader
+import pdf2image
 import os
+
+
 def segments_page_pdf_to_pdf():
     resPath = os.path.dirname(os.path.dirname(__file__))
     srcFile = os.path.join(os.path.normpath(resPath),'res','document','bangla_dictionary.pdf')
@@ -13,4 +16,20 @@ def segments_page_pdf_to_pdf():
         with open(outputFilename, "wb") as outputStream:
             output.write(outputStream)
 
-segments_page_pdf_to_pdf()
+def convert_pdf_to_image():
+    resPath = os.path.dirname(os.path.dirname(__file__))
+    srcFile = os.path.join(os.path.normpath(resPath), 'res', 'document', 'bangla_dictionary.pdf')
+    print(srcFile)
+    outputDir = os.path.join(resPath, 'res', 'page_images')
+    pages = pdf2image.convert_from_path(srcFile, 500, output_folder=outputDir, fmt="jpeg")
+    i=1
+    for page in pages:
+        print("Doing page %s" %i)
+        page.save(outputDir +"\page%s.jpg" % i, 'JPEG')
+        print("Done page %s" %i)
+        i= i+1
+
+#segments_page_pdf_to_pdf()
+convert_pdf_to_image()
+
+
